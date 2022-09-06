ac32d2d1c316b2912619ff063e3fd394cf2a7663
MegaC-C <59622754+MegaC-C@users.noreply.github.com>
2022-09-06 10:54:20 +0200

    added front and smd part detection

--- gCodeParser.cpp
+++ gCodeParser.cpp
@@ -8,6 +8,9 @@ using namespace std;
 
 string word;
 int modFlag = 0;
+int smdFlag = 0;
+int froFlag = 0;
+int firstPosFlag = 0;
 int posFlag = 0;
 int oriFlag = 0;
 int sizeFlag = 0;
@@ -36,22 +39,62 @@ int main ()
 	outfile << fixed;					// always print two decimals
 	outfile << setprecision(2);
 	
+	outfile << "M83 			;Set E to Relative Positioning" << endl;
+	outfile << "M92 E1.00 		;Set eSteps to 1 step/mm for better readibility of extrusion (e.g. G1 E3.00 will result in 3 microsteps)" << endl;
+	outfile << "M204 T10 		;Set travel acceleration to 10mm/s²" << endl;
+	outfile << "M204 P5 		;Set extrude acceleration to 5mm/s²" << endl;
+	outfile << "G0 F600 		;Set travel speed to 600 mm/min = 10 mm/s" << endl << endl;
+	
 	while(!infile.eof())				// stay in loop until end of file 
 	{
 		infile >> word;					// read every individual "word" and set flags for keywords 
 		if (word == "$MODULE")
 		{
-			modFlag = 1;
-			rotFlag = 0;
+			modFlag = 1;				// start new part and reset other Flags
+			smdFlag = 0;
+			froFlag = 0;
+			rotFlag = 0;	
 			outfile << ";MODULE ";
 			infile >> word;
 			outfile << word << endl;
 		}
-		else if (word == "position")
+		else if (word == "attribut")
+		{
+			infile >> word;
+			if (word == "smd") 
+			{
+				smdFlag = 1;
+				posFlag = 0;				// reset position and size Flags to enter routine not instantly after smdFlag was raised
+				sizeFlag = 0;	
+			}
+			else 
+			{
+				outfile << ";not smd" << endl;
+			}
+		}
+		else if (word == "layer")
+		{
+			infile >> word;
+			if (word == "front") 
+			{
+				froFlag = 1;
+				posFlag = 0;				// reset position and size Flags to enter routine not instantly after froFlag was raised
+				sizeFlag = 0;
+			}
+			else 
+			{
+				outfile << ";not front" << endl;
+			}
+		}
+		else if (word == "position" && modFlag == 1)		// first position in $MODULE is the center of the part
+		{
+			firstPosFlag = 1;
+		}
+		else if (word == "position" && modFlag == 0)		// subsequent positions give pad position relative to the center of the part
 		{
 			posFlag = 1;
 		}
-		else if (word == "orientation")
+		else if (word == "orientation" && modFlag == 1)		// only first "orientation" in $MODULE is relevant
 		{
 			oriFlag = 1;
 		}
@@ -60,10 +103,9 @@ int main ()
 			sizeFlag = 1;
 		}
 		
-		
-		if (posFlag == 1 && modFlag == 1)				// first position in $MODULE is the center of the part
+		if (firstPosFlag == 1)				
 		{
-			posFlag = 0;
+			firstPosFlag = 0;
 			outfile << ";positionMid ";
 			infile >> word;
 			xRef = stof(word);
@@ -71,38 +113,8 @@ int main ()
 			yRef = stof(word);							// store base values of x/y of this part until next part
 			outfile << xRef << " " << yRef;				// write as comment to gCode							
 		}
-		if (posFlag == 1 && modFlag == 0)				// all subsequent "positions" in $MODULE give x/y of the individual pads of the part with respect to the center
-		{
-			posFlag = 0;
-			outfile << "G0 Z30" << endl << "G0 X";
-			infile >> word;
-			x = stof(word);
-			infile >> word;
-			y = stof(word);
-			if (rotFlag == 1)
-			{
-				outfile << (xRef + y) << " Y" << (yRef + x) << endl << "G0 Z-30" << endl;	// depending on the orientation of the part x and y have to be switched
-			}
-			else 
-			{
-				outfile << (xRef + x) << " Y" << (yRef + y) << endl << "G0 Z-30" << endl;
-			}			
-		}
 		
-		
-		if (sizeFlag == 1)												// gives the size of each pad
-		{
-			sizeFlag = 0;
-			infile >> word;
-			xSize = stof(word);
-			infile >> word;
-			ySize = stof(word);
-			volume = ySize*xSize*zSize;									// required volume of solder paste per pad
-			outfile << "G1 E" << ceil(volume / volumePerStep) << endl;	// number of steps to extrude reqiured volume 			
-		}
-		
-		
-		if (oriFlag == 1 && modFlag == 1)								// only first "orientation" in $MODULE is relevant
+		if (oriFlag == 1)								
 		{
 			modFlag = 0;
 			oriFlag = 0;
@@ -115,10 +127,42 @@ int main ()
 			}
 			outfile << o << endl;
 		}
-		if (oriFlag == 1 && modFlag == 0)								// resets flag for nonFirst "orientation" in $MODULE
+		
+		
+		if (froFlag == 1 && smdFlag == 1)	// calculate solder paste extrusion only for smd parts on front layer
 		{
-			oriFlag = 0;
+			if (posFlag == 1)				// all subsequent "positions" in $MODULE give x/y of the individual pads of the part with respect to the center
+			{
+				posFlag = 0;
+				outfile << "G0 Z30.00" << endl << "G0 X";
+				infile >> word;
+				x = stof(word);
+				infile >> word;
+				y = stof(word);
+				if (rotFlag == 1)
+				{
+					outfile << (xRef + y) << " Y" << (yRef + x) << endl << "G0 Z0.01" << endl;	// depending on the orientation of the part x and y have to be switched
+				}
+				else 
+				{
+					outfile << (xRef + x) << " Y" << (yRef + y) << endl << "G0 Z0.01" << endl;
+				}			
+			}			
+			
+			if (sizeFlag == 1)																	// gives the size of each pad
+			{
+				sizeFlag = 0;
+				infile >> word;
+				xSize = stof(word);
+				infile >> word;
+				ySize = stof(word);
+				volume = ySize*xSize*zSize;														// required volume of solder paste per pad
+				outfile << "G0 F60 			;Set extrusion speed to 60 steps/min = 1 step/s" << endl;
+				outfile << "G1 E" << ceil(volume / volumePerStep) << endl;						// number of steps to extrude reqiured volume
+				outfile << "G0 F600 		;Set travel speed to 600 mm/min = 10 mm/s" << endl;
+			}
 		}
+			
 	}
 	
 	infile.close();
